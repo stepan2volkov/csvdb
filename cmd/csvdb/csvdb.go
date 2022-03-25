7bcb0a2e33cc910dc14dd1d88463fa18a75b27a8
Stepan Volkov <83796929+stepan2volkov@users.noreply.github.com>
2022-03-25 16:37:50 +0300

    [CSVDB-11] logging into files using zap (#10)
    * [CSVDB-10] Bug fixed
    
    * [CSVDB-11] logging into files using zap

--- cmd/csvdb/csvdb.go
+++ cmd/csvdb/csvdb.go
@@ -6,17 +6,24 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/stepan2volkov/csvdb/internal/app"
 	"github.com/stepan2volkov/csvdb/internal/app/table/formatter"
 	"github.com/stepan2volkov/csvdb/internal/app/table/loader"
+	"github.com/stepan2volkov/csvdb/internal/logger"
+	"go.uber.org/zap"
 )
 
 func main() {
+	log := logger.GetLogger()
+	log.Info("starting csv-db")
 	a := app.NewApp()
 	f := formatter.DefaultFormatter{}
+
 	scanner := bufio.NewScanner(os.Stdin)
 
+	log.Info("csv-db has been ready to accept queries")
 	for {
 		fmt.Print("~# ")
 
@@ -35,16 +42,18 @@ func main() {
 			in = strings.TrimSpace(strings.TrimPrefix(in, `\load`))
 			args := strings.Split(in, " ")
 			if len(args) != 2 {
-				fmt.Println("\tThe right syntax: \n\t\\load <csv-path> <config-path>")
+				fmt.Printf("wrong syntax for \\load: '%s'\n", in)
 				continue
 			}
 			t, err := loader.LoadFromCSV(args[0], args[1])
 			if err != nil {
-				fmt.Printf("ERROR: %v\n", err)
+				fmt.Printf("error when loading from csv: %v\n", err)
 				continue
 			}
 			if err = a.LoadTable(t); err != nil {
-				fmt.Printf("ERROR: %v\n", err)
+				fmt.Printf("error when loading table: %v\n", err)
+				log.Error("error when loading table",
+					zap.Error(err))
 			}
 		case in == `\list`:
 			fmt.Println(strings.Join(a.TableList(), "\n"))
@@ -52,19 +61,30 @@ func main() {
 		case strings.HasPrefix(in, `\drop`):
 			newTableName := strings.TrimSpace(strings.TrimPrefix(in, `\drop`))
 			if newTableName == "" {
-				fmt.Println("\tThe right syntax: \n\t\\drop <tablename>")
+				fmt.Printf("wrong syntax for \\drop: '%s'\n", in)
 				continue
 			}
 			if err := a.DropTable(newTableName); err != nil {
-				fmt.Printf("\t%v\n", err)
+				fmt.Printf("error when dropping table: '%v'\n", err)
+				log.Error("error when dropping table",
+					zap.Error(err))
 			}
 		default:
+			start := time.Now()
 			res, err := a.Execute(context.Background(), in)
 			if err != nil {
-				fmt.Printf("ERROR: %v\n", err)
+				fmt.Printf("error: %v\n", err)
+				log.Error("error executing query",
+					zap.String("query", in),
+					zap.Error(err))
 				continue
 			}
+			duration := time.Since(start)
 			fmt.Println(f.Format(res))
+			log.Info("query has been executed",
+				zap.String("query", in),
+				zap.String("table", res.Name),
+				zap.Duration("duration", duration))
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/jedib0t/go-pretty/v6 v6.2.7
 	github.com/stretchr/testify v1.7.0
+	go.uber.org/zap v1.21.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
 
@@ -15,6 +16,8 @@ require (
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 )
